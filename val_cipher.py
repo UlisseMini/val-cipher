6b3c08d31ac8a02a21b8a59745a6e7972c9d26a2
ScriptKidy <ulisse.mini@gmail.com>
2018-09-13 13:51:58 -0400

    Added some comments

--- val_cipher.py
+++ val_cipher.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 # Val cipher by valvate
+# Use generate.py to generate a key for yourself then replace the following:
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890 '
+key =      'F7dJ89aph5r40Rw6yUAzoqm897Ht0CBviWSD5Esbg2M2VQXGxcOlT6fjPnkuL4e1Y1IZ33KN '
 
 # Hex encoder #
 from base64 import b16decode, b16encode 
@@ -17,10 +21,6 @@ def hexdecode(data):
     decrypted = b16decode(data)
     decrypted = decrypted.decode('utf-8')
     return decrypted
-
-# Mono cipher basic subsitution
-alphabet = 'abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890 '
-key =      'F7dJ89aph5r40Rw6yUAzoqm897Ht0CBviWSD5Esbg2M2VQXGxcOlT6fjPnkuL4e1Y1IZ33KN '
 
 # Mono cipher #
 def encrypt(data):
