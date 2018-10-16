2c87cd2c4788a5d800b814a820a1830ca86f6b89
ScriptKidy <ulisse.mini@gmail.com>
2018-10-16 13:14:52 -0400

    Fixed bugo

--- val_cipher.py
+++ val_cipher.py
@@ -2,8 +2,8 @@
 # Val cipher by valvate
 # Use generate.py to generate a key for yourself then replace the following:
 
-alphabet = 'abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890 '
-key =      'F7dJ89aph5r40Rw6yUAzoqm897Ht0CBviWSD5Esbg2M2VQXGxcOlT6fjPnkuL4e1Y1IZ33KN '
+alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+key =      'wenKOT60gYRszjQCqm58ZJPELV7iUoh9INkXyvuMt3clSpWB2x4fHFAba1GrDd'
 
 # Hex encoder #
 from base64 import b16decode, b16encode 
@@ -24,14 +24,12 @@ def hexdecode(data):
 
 # Mono cipher #
 def encrypt(data):
-    # This works
     # Encodes data into base16
     data = hexencode(data)
     indexVals = [alphabet.index(char.lower()) for char in data]
     return ''.join(key[indexKey] for indexKey in indexVals)
 
 def decrypt(data):
-    # This does not
     indexVals = [key.index(char) for char in data]
     deciphered = ''.join(alphabet[indexKey] for indexKey in indexVals)
     deciphered = hexdecode(deciphered)
