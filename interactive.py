0be9f28103e9f4d5fafcf528a62165e920a7f5f2
ScriptKidy <ulisse.mini@gmail.com>
2018-10-16 13:36:17 -0400

    Made interactive better

--- interactive.py
+++ interactive.py
@@ -5,16 +5,15 @@ print("""
         2: Decrypt
         3: Exit""")
 choices = ["1", "2", "3"]
-
 choice = input("-> ")
-while choice not in choices:
+while choice != '3':
+    if choice == "1":
+        text = input("Text to encrypt\n-> ")
+        encrypted = val_cipher.encrypt(text)
+        print(encrypted)
+    if choice == "2":
+        text = input("Text to decrypt\n-> ")
+        decrypted = val_cipher.decrypt(text)
+        print(decrypted)
+    
     choice = input("-> ")
-
-if choice == "1":
-    text = input("Text to encrypt\n-> ")
-    encrypted = val_cipher.encrypt(text)
-    print(encrypted)
-if choice == "2":
-    text = input("Text to decrypt\n-> ")
-    decrypted = val_cipher.decrypt(text)
-    print(decrypted)
